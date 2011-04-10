27bf704d3a792e87b4ac90d15833d6efeea3b6db
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2011-04-10 16:48:28 -0600

    All the basic functions implemented.

--- sudoku2tikz.py
+++ sudoku2tikz.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python
-import string
+import os.path
+import tempfile
+import shutil
+from optparse import OptionParser
+from subprocess import call
 from pprint import pprint
 
-header = '''
-\\begin{tikzpicture}
-\\draw[gray] (0,0) grid [step=1] (9,9);
-\\draw[thick] (0,0) grid [step=3] (9,9);
+header = r'''\begin{tikzpicture}
+\draw[gray] (0,0) grid [step=1] (9,9);
+\draw[thick] (0,0) grid [step=3] (9,9);
 '''
-trail = '\\end{tikzpicture}'
-cell = '\\node at (%(x).1f,%(y).1f) {\\Large{%(c)s}};'
-cell_solution = '\\node at (%(x).1f,%(y).1f) {\\Large{\\textcolor{red}{%(c)s}}};'
+footnote = r'\node[anchor=north west] at (0,0) {\tiny{\textsf{Start position in black, solution in red}}};'
+trail = r'''\end{tikzpicture}'''
+cell = r'\node at (%(x).1f,%(y).1f) {\Large{%(c)s}};'
+cell_solution = r'\node at (%(x).1f,%(y).1f) {\Large{\textcolor{red}{%(c)s}}};'
 
 N = 9 # For the moment, we fix the dimension to 9x9
 
-def parse_sudoku(p, sep):
+latex_header = r'''
+\documentclass{article}
+\usepackage{tikz,amsmath,siunitx}
+\usetikzlibrary{arrows,snakes,backgrounds,patterns,matrix,shapes,fit,calc,shadows,plotmarks}
+\usepackage[graphics,tightpage,active]{preview}
+\PreviewEnvironment{tikzpicture}
+\PreviewEnvironment{equation}
+\PreviewEnvironment{equation*}
+\newlength{\imagewidth}
+\newlength{\imagescale}
+\pagestyle{empty}
+\thispagestyle{empty}
+\begin{document}'''
+
+latex_trail = r'''
+\end{document}
+'''
+
+
+def parse_sudoku(p):
+    if p.count('|'):
+        sep = '|'
+    else:
+        sep = ' '
     d = [[e for e in line.split(sep) if len(e)>0] for line in p.split('\n')]
     d = [r for r in d if len(r)>0]
     d.reverse()
@@ -33,48 +60,52 @@ def get_tikz_lines(d):
             
     return lines
 
-def save_tikz(p, file_name, sep=' '):
-    d = parse_sudoku(p, sep)
+def get_tikz(d, options):
     tikz_lines = get_tikz_lines(d)
-    s = header + '\n'.join(tikz_lines) + '\n' + trail + '\n'
-    f = open(file_name, 'w')
-    f.write(s)
-    f.close()
+    if options.footnote:
+        ft = footnote
+    else:
+        ft = ''
+    parts = [header, '\n'.join(tikz_lines), ft, trail]
+    s = '\n'.join(parts) + '\n'
+    return s
     
-p1 = '''
-9 . . 4 . . . 1 6
-2 . . . 5 6 7 . .
-. . . 8 7 1 4 . 2
-6 3 . . 1 . . 5 .
-. . . . 8 . . . . 
-. 7 . . 3 . . 4 9 
-7 . 6 2 4 8 . . .
-. . 8 3 9 . . . 4 
-3 9 . . . 7 . . 5
-'''
-
-p2 = '''
-|5|3| | |7| | | | |.
-|6| | |1|9|5| | | |.
-| |9|8| | | | |6| |.
-|8| | | |6| | | |3|.
-|4| | |8| |3| | |1|.
-|7| | | |2| | | |6|.
-| |6| | | | |2|8| |.
-| | | |4|1|9| | |5|.
-| | | | |8| | |7|9|.'''
-
-p2s = '''
-|5|3|4s|6s|7|8s|9s|1s|2s|.
-|6|7s|2s|1|9|5|3s|4s|8s|.
-|1s|9|8|3s|4s|2s|5s|6|7s|.
-|8|5s|9s|7s|6|1s|4s|2s|3|.
-|4|2s|6s|8|5s|3|7s|9s|1|.
-|7|1s|3s|9s|2|4s|8s|5s|6|.
-|9s|6|1s|5s|3s|7s|2|8|4s|.
-|2s|8s|7s|4|1|9|6s|3s|5|.
-|3s|4s|5s|2s|8|6s|1s|7|9|.'''
-
+def make_pdf(tikz, file_name):
+    latex = latex_header + tikz + latex_trail
+    tmp_dir = tempfile.mkdtemp()
+    base_latex_file = os.path.join(tmp_dir, file_name)
+    latex_file = base_latex_file + '.tex'
+    f = open(latex_file, 'w')
+    f.write(latex)
+    f.close()
+    call(('pdflatex', latex_file), cwd=tmp_dir)
+    shutil.copyfile(base_latex_file + '.pdf', file_name + '.pdf')
+    shutil.rmtree(tmp_dir)
+    
 if __name__ == '__main__':
-    save_tikz(p2, 'sudoku2.tikz', '|')
-    save_tikz(p2s, 'sudoku2s.tikz', '|')
+    parser = OptionParser()
+    parser.add_option('-t', action='store_false', dest='out_tikz',
+                      default=True, help='Output tikz file')
+    parser.add_option('-p', action='store_true', dest='out_pdf',
+                      default=False, help='Output PDF file')
+    parser.add_option('-f', action='store_true', dest='footnote',
+                      default=False, help='Add footnote')
+    (options, args) = parser.parse_args()
+    
+    for file_name in args:
+        try:
+            f = open(file_name, 'r')
+        except IOError:
+            print "The file", f, "doesn't exist."
+        else:
+            p = f.read()
+            d = parse_sudoku(p)
+            f.close()
+            base_name = os.path.splitext(file_name)[0]
+            tikz = get_tikz(d, options)
+            if options.out_tikz:
+                f = open(base_name + '.tikz', 'w')
+                f.write(tikz)
+                f.close()
+            if options.out_pdf:
+                make_pdf(tikz, base_name)
